b76042ce0e25072edd5a70908f032f1ea37ecc17
jasiek123 <janwiecinski@wp.pl>
2018-05-03 16:49:21 +0200

    about contact

---
+++ CookingSchool.Portal/Controllers/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using CookingSchool.Portal.Models;
+using System.Web.Mvc;
+
+namespace CookingSchool.Portal.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult Index()
+        {
+            return View("About");
+        }
+
+        public ActionResult SendContactMessage( ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+            }
+            return View();
+        }
+    }
+}
